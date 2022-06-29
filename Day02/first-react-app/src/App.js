8b0fc8cffdb0bc7c048c3e1ff3fd11d377491388
jhyunlee94 <jkwhd12@gmail.com>
2022-06-29 11:35:02 +0900

    0629 11:30

--- Day02/first-react-app/src/App.js
+++ Day02/first-react-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloReact from './component/HelloReact';
+import DummyComponent from './component/DummyComponent';
+import PropsOfComponent from './component/PropsOfComponent';
 
 function App() {
-  return (
-    <HelloReact/>
-  );
+  return <PropsOfComponent name="REACT" />;
 }
 
 export default App;
--- Day02/first-react-app/src/component/HelloReact.js
+++ Day02/first-react-app/src/component/HelloReact.js
@@ -1,11 +1,29 @@
 import React, { Fragment } from 'react';
+import '../App.css';
 
 const HelloReact = () => {
     const name = "리액트";
+    // const style = {
+    //     backgroundColor: "black",
+    //     color: "white",
+    //     fontSize: "48px",
+    //     fontWeight: "bold",
+    //     padding: 16
+    // }
     return (
         <Fragment>
-            <h1>Hello {name} React!!</h1>
-            <h2>First {name} App</h2>
+            {/* {name === "리액트" ? (
+                <h1>리액트입니다.</h1>
+            ) : (
+                null
+            )}  */
+            
+            name === "리액트" && <h1 className='react'>리액트입니다.</h1>
+            }
+            <h1 className='react'>{name}</h1>
+            {/* 주석은 이렇게 사용한다. */}
+            //이런 주석은 다 표출됨
+            /* 이런 주석은 다 표출됨 */
         </Fragment>
     );
 };
