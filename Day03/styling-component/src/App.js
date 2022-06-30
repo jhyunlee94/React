9001f8aee78d217e90cd08a05a608df319d92302
jhyunlee94 <jkwhd12@gmail.com>
2022-06-30 16:33:44 +0900

    0630 Day03

---
+++ Day03/styling-component/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SassComponent from "./components/SassComponent";
+import CSSModule from "./components/CSSModule";
+import StyledComponent from "./components/StyledComponent";
+
+function App() {
+  return (
+    <div>
+      <StyledComponent></StyledComponent>
+    </div>
+  );
+}
+
+export default App;
