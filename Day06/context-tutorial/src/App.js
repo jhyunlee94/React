79e08e8da12078dfc4031311f1d8d135ed3e1c81
jhyunlee94 <jkwhd12@gmail.com>
2022-07-05 15:04:40 +0900

    0705 15:00

---
+++ Day06/context-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import ColorBox from "./components/ColorBox";
+import { ColorProvider } from "./contexts/color";
+
+function App() {
+  return (
+    <ColorProvider>
+      <div>
+        <ColorBox></ColorBox>
+      </div>
+    </ColorProvider>
+  );
+}
+
+export default App;
---
+++ Day06/context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ColorContext, { ColorConsumer } from "../contexts/color";
+
+const ColorBox = () => {
+  return (
+    <ColorConsumer>
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          >
+            {/* <p style={{ color: state.fontColor }}>React</p> */}
+          </div>
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.fontColor,
+            }}
+          >
+            {/* <p style={{ color: state.fontColor }}>React</p> */}
+          </div>
+        </>
+      )}
+    </ColorConsumer>
+  );
+};
+
+export default ColorBox;
