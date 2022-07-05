e7c46ee12b8e0d56d108e2977b3116ae94eeed67
jhyunlee94 <jkwhd12@gmail.com>
2022-07-05 16:02:09 +0900

    0705 Day06

--- Day06/context-tutorial/src/App.js
+++ Day06/context-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
 import { ColorProvider } from "./contexts/color";
 
 function App() {
   return (
     <ColorProvider>
       <div>
+        <SelectColors></SelectColors>
         <ColorBox></ColorBox>
       </div>
     </ColorProvider>
--- Day06/context-tutorial/src/components/ColorBox.js
+++ Day06/context-tutorial/src/components/ColorBox.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import ColorContext, { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          >
-            {/* <p style={{ color: state.fontColor }}>React</p> */}
-          </div>
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.fontColor,
-            }}
-          >
-            {/* <p style={{ color: state.fontColor }}>React</p> */}
-          </div>
-        </>
-      )}
-    </ColorConsumer>
+    // <ColorConsumer>
+    // {({ state }) => (
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      >
+        {/* <p style={{ color: state.fontColor }}>React</p> */}
+      </div>
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.fontColor,
+        }}
+      >
+        {/* <p style={{ color: state.fontColor }}>React</p> */}
+      </div>
+    </>
+    // )}
+    // </ColorConsumer>
   );
 };
 
