331b0c5a890abadd79fcc344e8f4e64cd02ca7ce
jhyunlee94 <jkwhd12@gmail.com>
2022-07-06 16:57:25 +0900

    0706

---
+++ Day07/redux-middleware-tutorial/src/containers/SampleContainer.js
@@ -0,0 +1,43 @@
+import { connect } from "react-redux";
+import Sample from "../components/Sample";
+import { getPost, getUsers } from "../lib/api";
+import { useEffect } from "react";
+import { bindActionCreators } from "redux";
+const SampleContainer = ({
+  getPost,
+  getUsers,
+  post,
+  users,
+  loadingPost,
+  loadingUsers,
+}) => {
+  useEffect(() => {
+    getPost(1);
+    getUsers();
+  }, [getPost, getUsers]);
+  return (
+    <Sample
+      post={post}
+      users={users}
+      loadingPost={loadingPost}
+      loadingUsers={loadingUsers}
+    />
+  );
+};
+
+export default connect(
+  (state) => ({
+    post: state.sample.post,
+    users: state.sample.users,
+    loadingPost: state.sample.loading.GET_POST,
+    loadignUsers: state.sample.loading.GET_USERS,
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        getPost,
+        getUsers,
+      },
+      dispatch
+    )
+)(SampleContainer);
--- Day07/redux-middleware-tutorial/src/index.js
+++ Day07/redux-middleware-tutorial/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
