d73e59d2cf467d9affb63287c198eaca3a951f03
jhyunlee94 <jkwhd12@gmail.com>
2022-07-06 14:07:43 +0900

    0706

--- Day06/redux-tutorial/src/containers/TodosContainer.js
+++ Day06/redux-tutorial/src/containers/TodosContainer.js
@@ -1,42 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
+import { useSelector, useDispatch } from 'react-redux';
+import { useCallback } from 'react';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector((state) => ({
+    input: state.todos.input,
+    todos: state.todos.todos,
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (input) => dispatch(changeInput(input)),
+    [dispatch],
+  );
+  const onIsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onIsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  (state) => ({
-    input: state.todos.input,
-    todos: state.todos.todos,
-  }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        changeInput,
-        insert,
-        toggle,
-        remove,
-      },
-      dispatch,
-    ),
-)(TodosContainer);
+export default React.memo(TodosContainer);
--- Day06/redux-tutorial/src/modules/counter.js
+++ Day06/redux-tutorial/src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction, handleActions } from 'redux-actions';
+
 //1. 액션 타입을 정의한다.
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 //2. 액션 생성 함수를 정의한다.
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //3. counter 모듈의 초기상태를 정의한다.
 const initialState = {
@@ -12,19 +14,12 @@ const initialState = {
 };
 
 //4. 리듀서 함수를 정의한다.
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
